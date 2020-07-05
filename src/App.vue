e004531255a14eb5aefd6369dbb6231089c60652
TANISHQ CHAMOLA <36632821+tanishqchamola@users.noreply.github.com>
2020-07-05 22:06:33 +0530

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h1 style="text-align: center">Vuex Todo Manager</h1>
+    <div class="container">
+      <div class="row">
+        <AddTodo />
+      </div>
+      <div class="row">
+        <FilterTodo />
+      </div>
+      <div class="row">
+        <Todos />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Todos from "@/components/Todos.vue";
+import AddTodo from "@/components/AddTodo.vue";
+import FilterTodo from "@/components/FilterTodo.vue";
+export default {
+  name: "app",
+  components: {
+    Todos,
+    AddTodo,
+    FilterTodo
+  }
+};
+</script>
+
+<style>
+body {
+  font-family: "Open Sans", Arial, sans-serif;
+  line-height: 1.6;
+  background-color: #e8f7f0;
+}
+.container {
+  max-width: 1100px;
+  margin: auto;
+  overflow: auto;
+  padding: 0 2rem;
+}
+.row {
+  margin-top: 10px;
+  margin-bottom: 15px;
+}
+</style>
---
+++ src/components/FilterTodo.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        Filter Todos:
+        <select @change="filterTodo($event)">
+            <option value="200">200</option>
+            <option value="100">100</option>
+            <option value="50">50</option>
+            <option value="20">20</option>
+            <option value="10">10</option>
+        </select>
+    </div>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+export default {
+    name: "FilterTodos",
+    methods: mapActions(['filterTodo'])
+}
+</script>
+
+<style scoped>
+
+</style>
